5fa8a4d4c0a8b02f1a1d9af04c973d5ee41609a1
Evan Cui <evan@good-meowning.com>
2023-10-28 09:41:00 -0400

    transport key binding and clicking

--- src/contents/judging-ui.svelte
+++ src/contents/judging-ui.svelte
@@ -40,13 +40,72 @@
 </script>
 
 <script lang="ts">
+  import { Storage } from "@plasmohq/storage";
+
   import { isActivated } from "./store";
 
   // activate/deactivate ui
   let activated: boolean;
-  isActivated.subscribe((value) => {
-    activated = value;
+  isActivated.subscribe((value) => (activated = value));
+
+  // browser storage
+  const storage = new Storage();
+
+  // key bindings
+  let positiveKey: string;
+  let negativeKey: string;
+
+  // get default keys iife
+  (async () => {
+    positiveKey = await storage.get("positiveKey");
+    negativeKey = await storage.get("negativeKey");
+    console.debug(`init get keys "${positiveKey}", "${negativeKey}"`);
+
+    // some key is missing from storage, reset to default
+    if (!positiveKey || !negativeKey) {
+      console.debug("reset keys");
+      positiveKey = "1";
+      negativeKey = "0";
+      storage.set("positiveKey", positiveKey);
+      storage.set("negativeKey", negativeKey);
+    }
+  })();
+
+  // watch for key binding changes
+  storage.watch({
+    positiveKey: (c) => {
+      console.debug(`positiveKey: ${c.newValue}`);
+      positiveKey = c.newValue;
+    },
+    negativeKey: (c) => {
+      console.debug(`negativeKey: ${c.newValue}`);
+      negativeKey = c.newValue;
+    }
   });
+
+  // listen for clicks
+  document.addEventListener(
+    "keydown",
+    (event) => {
+      // do nothing if scoring has not begun
+      if (!activated) {
+        return;
+      }
+
+      if (event.key === positiveKey || event.key === negativeKey) {
+        // disable default key actions
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        const click = event.key === positiveKey ? +1 : -1;
+        console.debug(`click ${click}`);
+      }
+    },
+    // capture prioritizes this event listener it's rly kool
+    // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#capture
+    { capture: true }
+  );
 </script>
 
 <div
--- src/popup.svelte
+++ src/popup.svelte
@@ -1,6 +1,66 @@
 <script lang="ts">
-  let positiveKey = "";
-  let negativeKey = "";
+  import { Storage } from "@plasmohq/storage";
+
+  const storage = new Storage();
+
+  let positiveNode: HTMLInputElement;
+  let negativeNode: HTMLInputElement;
+  let positiveKey: string;
+  let negativeKey: string;
+
+  // watch for key binding changes
+  storage.watch({
+    positiveKey: (c) => {
+      console.debug(`positiveKey: ${c.newValue}`);
+      positiveKey = c.newValue;
+    },
+    negativeKey: (c) => {
+      console.debug(`negativeKey: ${c.newValue}`);
+      negativeKey = c.newValue;
+    }
+  });
+
+  const updateKeys = () => {
+    // check if keys are not unique
+    if (positiveKey === negativeKey) {
+      console.debug(`duplicate key binds "${positiveKey}", "${negativeKey}"`);
+      negativeNode.setCustomValidity("Invalid duplicate key bind!");
+      positiveNode.setCustomValidity("Invalid duplicate key bind!");
+    } else {
+      // check if keys are valid, and reset custom validity
+      negativeNode.setCustomValidity(
+        /^.{1}$/.test(negativeNode.value) ? "" : "Invalid key bind!"
+      );
+      positiveNode.setCustomValidity(
+        /^.{1}$/.test(positiveNode.value) ? "" : "Invalid key bind!"
+      );
+      console.debug(
+        `validity "${positiveNode.checkValidity()}", "${negativeNode.checkValidity()}"`
+      );
+
+      // save keys if valid
+      if (positiveNode.checkValidity() && negativeNode.checkValidity()) {
+        console.debug(`store keys "${positiveKey}", "${negativeKey}"`);
+        storage.set("positiveKey", positiveKey);
+        storage.set("negativeKey", negativeKey);
+      }
+    }
+  };
+
+  // get default keys iife
+  (async () => {
+    positiveKey = await storage.get("positiveKey");
+    negativeKey = await storage.get("negativeKey");
+    console.debug(`init get keys "${positiveKey}", "${negativeKey}"`);
+
+    // some key is missing from storage, reset to default
+    if (!positiveKey || !negativeKey) {
+      console.debug("reset keys");
+      positiveKey = "1";
+      negativeKey = "0";
+      updateKeys();
+    }
+  })();
 </script>
 
 <p>Edit your clicking keys!</p>
@@ -8,13 +68,15 @@
 <div>
   <label for="pos-key">Positive (+1): </label>
   <input
+    bind:this={positiveNode}
     bind:value={positiveKey}
+    on:input={updateKeys}
     id="pos-key"
     name="pos-key"
     type="text"
     minlength="1"
     maxlength="1"
-    size="2"
+    pattern={"^.{1}$"}
     required
   />
   <span class="validity" />
@@ -23,13 +85,15 @@
 <div>
   <label for="neg-key">Negative (-1): </label>
   <input
+    bind:this={negativeNode}
     bind:value={negativeKey}
+    on:input={updateKeys}
     id="neg-key"
     name="neg-key"
     type="text"
     minlength="1"
     maxlength="1"
-    size="2"
+    pattern={"^.{1}$"}
     required
   />
   <span class="validity" />
